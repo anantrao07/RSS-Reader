aa2be4a7da445cc765e32da3d58da7d5888ead7a
anant <anant.rao07@gmail.com>
2017-09-08 18:29:34 -0300

    replacing the expired json url with new one

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -9,7 +9,8 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+            //url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+            url = new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/March/58c5d68f_xyz-reader/xyz-reader.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
         }
